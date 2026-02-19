06af848a080e214bf059b2655e83728ae3ebc914
tnahddisttud <siddhantp73@gmail.com>
2026-02-19 18:42:02 +0530

    MINDSQL-24: Popped model param from config and added to llm instance

--- mindsql/llms/open_ai.py
+++ mindsql/llms/open_ai.py
@@ -25,6 +25,9 @@ class OpenAi(ILlm):
 
         if 'api_key' not in config:
             raise ValueError(OPENAI_VALUE_ERROR)
+        if 'model' in config:
+            self.model = config.pop('model')
+
         api_key = config.pop('api_key')
         self.client = OpenAI(api_key=api_key, **config)
 
@@ -79,7 +82,7 @@ class OpenAi(ILlm):
         if prompt is None or len(prompt) == 0:
             raise Exception(PROMPT_EMPTY_EXCEPTION)
 
-        model = self.config.get("model", "gpt-3.5-turbo")
+        model = self.model if self.model is not None else "gpt-5-2025-08-07"
         temperature = kwargs.get("temperature", 0.1)
         max_tokens = kwargs.get("max_tokens", 500)
         response = self.client.chat.completions.create(model=model, messages=[{"role": "user", "content": prompt}],
